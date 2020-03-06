7f79c158ffd01a183b5bc8c168bc81baf6e6d077
Bruno <bruno.b.souza@outlook.com>
2020-03-05 22:00:32 -0300

    criando parte de notificaçao e cancelamentos

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  secure: false,
+  port: 2525,
+  auth: {
+    user: '864fc42104ce71',
+    pass: 'bdd5650264daa5',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreplay@gobarber.com>',
+  },
+};
